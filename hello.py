0c04eb7b3fab34328912ab146b13f7fe08044c27
Lewisb1996 <lewisb76@icloud.com>
2018-10-15 11:32:00 +0100

    added new files

--- hello.py
+++ hello.py
@@ -3,7 +3,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('base.html')
+    return render_template('base.html'), 200
+
+@app.route('/continent/<string:name>')
+def continent(name):
+    #return name
+	return render_template(name+'.html')
+
+@app.route('/Africa')
+def africa():
+    return render_template('africa.html')
+
+@app.route('/Asia')
+def asia():
+    return render_template('asia.html')
+
+@app.route('/America')
+def america():
+    return render_template('america.html')
 
 if __name__ == ("__main__"):
     app.run(host='0.0.0.0', debug=True)
