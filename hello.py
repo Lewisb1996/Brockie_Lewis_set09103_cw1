cc7e51a105507352340084df2d8476de60d9d818
Lewisb1996 <lewisb76@icloud.com>
2018-10-19 12:55:19 +0100

    New files created and added

--- hello.py
+++ hello.py
@@ -10,18 +10,6 @@ def continent(name):
     #return name
 	return render_template(name+'.html')
 
-@app.route('/Africa')
-def africa():
-    return render_template('africa.html')
-
-@app.route('/Asia')
-def asia():
-    return render_template('asia.html')
-
-@app.route('/America')
-def america():
-    return render_template('america.html')
-
 if __name__ == ("__main__"):
     app.run(host='0.0.0.0', debug=True)
 
